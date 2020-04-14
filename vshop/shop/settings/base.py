003f51d993045a74c7d44be0ad1f52d64b6ffba4
natkhosh <t9219788607@gmail.com>
2020-04-14 20:12:46 +0300

    lab_3: > step 1

--- vshop/shop/settings/base.py
+++ vshop/shop/settings/base.py
@@ -1,3 +1,14 @@
 
 
-PHONE_NUMBER = '+7 (812) 703-02-02'
+PHONE_NUMBER = '+7 (812) 555-55-55'
+E_MAIL = 'veganbestfood@gmail.com'
+DAILY_OFFER = '!!! Free delivery !!!'
+
+TITLE = 'veganbestfood'
+
+ABOUT = 'Veganism is the practice of abstaining from the use of animal products and an associated philosophy ' \
+        'that rejects the commodity status of animals.'
+
+CONTACTS = 'Contacts'
+
+ADDRESS = '191025 Saint-Petersburg Nevsky pr. 122'
--- vshop/shop/views.py
+++ vshop/shop/views.py
@@ -5,10 +5,14 @@ from .settings.base import *
 
 # Create your views here.
 
+
 class IndexView(View):
 
     def get(self, request):
-        return render(request, 'shop/index.html', {'phone_number': PHONE_NUMBER})
+        return render(request, 'shop/index.html', {'phone_number': PHONE_NUMBER, 'e_mail': E_MAIL,
+                                                   'daily_offer': DAILY_OFFER, 'title': TITLE, 'about': ABOUT,
+                                                   'contacts': CONTACTS, 'address': ADDRESS})
+
 
 class ShopView(View):
 
