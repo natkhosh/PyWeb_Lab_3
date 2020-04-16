220ce9fe03c5b313172af531efc1e07ecbbff352
natkhosh <t9219788607@gmail.com>
2020-04-16 16:15:45 +0300

    lab_3: > step 5

--- vshop/shop/views.py
+++ vshop/shop/views.py
@@ -22,9 +22,52 @@ class IndexView(View):
 class ShopView(View):
 
     def get(self, request):
+        products = [{'name': 'Bell Pepper',
+                     'image': 'shop/images/product-1.jpg',
+                     'price': '$120.00',
+                     'discount': '30%',
+                     'price_sale': '$80.00'},
+                    {'name': 'Strawberry',
+                     'image': 'shop/images/product-2.jpg',
+                     'price': '$120.00'},
+                    {'name': 'Green Beans',
+                     'image': 'shop/images/product-3.jpg',
+                     'price': '$120.00'},
+                    {'name': 'Purple Cabbage',
+                     'image': 'shop/images/product-4.jpg',
+                     'price': '$120.00'},
+                    {'name': 'Tomatoe',
+                     'image': 'shop/images/product-5.jpg',
+                     'price': '$120.00',
+                     'discount': '30%',
+                     'price_sale': '$80.00'},
+                    {'name': 'Brocolli',
+                     'image': 'shop/images/product-6.jpg',
+                     'price': '$120.00'},
+                    {'name': 'Carrots',
+                     'image': 'shop/images/product-7.jpg',
+                     'price': '$120.00'},
+                    {'name': 'Fruit Juice',
+                     'image': 'shop/images/product-8.jpg',
+                     'price': '$120.00'},
+                    {'name': 'Onion',
+                     'image': 'shop/images/product-9.jpg',
+                     'price': '$120.00',
+                     'discount': '30%',
+                     'price_sale': '$80.00'},
+                    {'name': 'Apple',
+                     'image': 'shop/images/product-10.jpg',
+                     'price': '$120.00'},
+                    {'name': 'Garlic',
+                     'image': 'shop/images/product-11.jpg',
+                     'price': '$120.00'},
+                    {'name': 'Chilli',
+                     'image': 'shop/images/product-12.jpg',
+                     'price': '$120.00'}]
+
         return render(request, 'shop/shop.html',  {'phone_number': PHONE_NUMBER, 'e_mail': E_MAIL,
                                                    'daily_offer': DAILY_OFFER, 'title': TITLE, 'about': ABOUT,
-                                                   'contacts': CONTACTS, 'address': ADDRESS})
+                                                   'contacts': CONTACTS, 'address': ADDRESS, 'products': products})
 
 
 class WishlistView(View):
